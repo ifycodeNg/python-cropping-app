5449c241f025b1306ea5e5cb71a29275553df9a4
IfycodeN <ifyugwumba22@gmail.com>
2022-12-07 13:18:44 +0100

    rotate image

--- croppingApp/views.py
+++ croppingApp/views.py
@@ -1,26 +1,8 @@
 
-# from django.shortcuts import render, redirect
-
-# from .models import Photo
-# from .forms import PhotoForm
-
-
-# def photo_list(request):
-#     photos = Photo.objects.all()
-#     if request.method == 'POST':
-#         form = PhotoForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('photo_list')
-#     else:
-#         form = PhotoForm()
-#     return render(request, 'photo_list.html', {'form': form, 'photos': photos})
-
-
-
 
 from django.shortcuts import render, redirect
 from PIL import Image
+import uuid
 from .models import Photo
 
 def photo_list(request):
@@ -30,13 +12,16 @@ def photo_list(request):
         y = float(request.POST.get('y',False))
         w = float(request.POST.get('width',False))
         h = float(request.POST.get('height',False))
+        rotation = int(request.POST.get('rotation',False)) * -1
         category = request.POST.get('category',False)
         picture=request.FILES.get('image') 
-        photo= Photo(file=picture.name,category=category)      
+        image_name=str(uuid.uuid4())+'.'+picture.name.split(".")[-1]
+        photo= Photo(file=image_name,category=category)     
         photo.save()
         image = Image.open(picture)
         cropped_image=image.crop((x, y, w+x, h+y))
-        resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS) 
+        rotated_image=cropped_image.rotate(rotation)
+        resized_image = rotated_image.resize((200, 200), Image.ANTIALIAS) 
         resized_image.save(photo.file.path)
         return redirect('/')
     else:
